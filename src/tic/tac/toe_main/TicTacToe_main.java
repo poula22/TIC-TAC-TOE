d723419055791222c95215e7aad222b0785d58a0
poula22 <poula.george@yahoo.com>
2021-07-05 01:17:11 +0200

    first commit

---
+++ src/tic/tac/toe_main/TicTacToe_main.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic.tac.toe_main;
+
+
+
+
+
+/**
+ *
+ * @author george
+ */
+public class TicTacToe_main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+     TicTacToe.main(args);
+    }
+    
+}
